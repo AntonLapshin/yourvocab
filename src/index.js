f6e4221afc2cda0de9ee34dfa99ce9e67c62bde5
Anton Lapshin <anton.lapshin@sixt.com>
2018-08-25 13:33:58 +0200

    add trie

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { Trie, createTrieFromArray } from "trie";
+import debounce from "lodash/debounce";
+
 !(() => {
   if (window.__yv && window.__yv.initialized) {
     window.__yv.toggle();
@@ -5,7 +8,8 @@
   }
 
   const global = (window.__yv = {
-    state: false
+    state: false,
+    trie: null
   });
 
   //
@@ -15,6 +19,28 @@
     global.state = !global.state;
     console.log(global.state);
   };
+
+  global.save = debounce(() => {
+    const words = global.trie.getWords();
+    console.log("save:", words);
+    chrome.storage.local.set({
+      words
+    });
+  }, 5000);
+
+  //
+  // Initialization
+  //
+  chrome.storage.local.get("words", ({ words }) => {
+    if (!words) {
+      global.trie = new Trie();
+      // global.trie.addWord("home");
+      global.save();
+    } else {
+      console.log("load:", words);
+      global.trie = createTrieFromArray(words);
+    }
+  });
 
   global.initialized = true;
   global.toggle();
